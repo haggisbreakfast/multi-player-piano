7365281423528411137ccb1c9e0d3c44d4ecb6dd
Haggis Breakfast <lj.stewart00@gmail.com>
2018-11-12 15:35:42 -0800

    file types to jsx

---
+++ multi-player-piano/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>$up werld</p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
