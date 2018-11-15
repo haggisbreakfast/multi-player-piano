7f5e3a8791d574f9ebeb187b03f0d314e64e70f4
Haggis Breakfast <lj.stewart00@gmail.com>
2018-11-15 14:55:25 -0800

    receives data from websocket server

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,40 @@ const WEB_SOCKET_URL = process.env.REACT_APP_WEB_SOCKET_URL
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      note: '',
+    };
     // create a websocket connection to our server
     this.socket = new WebSocket(WEB_SOCKET_URL);
     // this.addMessage = this.addMessage.bind(this);
     console.log(WEB_SOCKET_URL);
   }
+  // function to send new note to websocket server
+  notePlayed = (note) => {
+    const newNote = {
+      note: this.state.note,
+    };
+    this.socket.send(JSON.stringify(newNote));
+  };
+
+  // loads below once component mounted to DOM
+  componentDidMount() {
+    // when connection with websockets is open..
+    this.socket.onopen = () => {
+      console.log('Connected');
+    };
+    // receiving data from websocket server
+    this.socket.onmessage = (event) => {
+      this.setState({
+        note: event.data.note,
+      });
+    };
+  }
 
   render() {
     return (
       <div className="App">
-        <Keys />
+        <Keys note={this.state.note} notePlayed={this.notePlayed} />
       </div>
     );
   }
