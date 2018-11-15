843ce6c3e5ddb72b348ff94810d9248d9835fe35
Haggis Breakfast <lj.stewart00@gmail.com>
2018-11-15 12:30:44 -0800

    adds function to log that note played on click

--- src/Keys.jsx
+++ src/Keys.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 class Keys extends React.Component {
+  keyClick = (event) => {
+    event.preventDefault();
+    console.log('note played');
+  };
   render() {
     return (
       <div className="Keys">
-        <div className="piano-key">
+        <div className="piano-key" onClick={this.keyClick}>
           <div className="white-key" id="C" />
         </div>
       </div>
