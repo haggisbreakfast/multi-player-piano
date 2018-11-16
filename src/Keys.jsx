8487ada449fb5a27a12c81d0fe968567d18de229
Haggis Breakfast <lj.stewart00@gmail.com>
2018-11-16 14:59:30 -0800

    adds key component

--- src/Keys.jsx
+++ src/Keys.jsx
@@ -1,35 +1,56 @@
 import React from 'react';
 
-class Keys extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      notes: {
-        C: 'c',
-        Csharp: 'c#',
-        D: 'd',
-        Dsharp: 'd#',
-        E: 'e',
-        F: 'f',
-        Fsharp: 'f#',
-        G: 'g',
-        Gsharp: 'g#',
-        A: 'a',
-        Asharp: 'a#',
-        B: 'b',
-      },
-      recordedNotes: [],
-    };
-  }
+class Key extends React.Component {
   keyClick = (event) => {
     event.preventDefault();
     console.log(event.target.id);
   };
   render() {
     return (
+      <div onClick={this.keyClick} className="white-key" id={this.props.note.name}>
+        {this.props.note.name}
+      </div>
+    );
+  }
+}
+
+class Keys extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recordednotes: [],
+
+      notes2: [
+        {
+          name: 'C',
+        },
+        {
+          name: 'C#',
+        },
+        {
+          name: 'D',
+        },
+        {
+          name: 'D#',
+        },
+        {
+          name: 'E',
+        },
+        {
+          name: 'F',
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
       <div className="Keys">
-        <div className="piano-key" onClick={this.keyClick}>
-          <div className="white-key" id={this.state.notes.C} />
+        <div className="piano-key">
+          {this.state.notes2.map((note, index) => {
+            return <Key note={note} key={index} />;
+          })}
+          {/* <div className="white-key" id={this.state.notes.C} />
           <div className="black-key" id={this.state.notes.Csharp} />
           <div className="white-key" id={this.state.notes.D} />
           <div className="black-key" id={this.state.notes.Dsharp} />
@@ -40,7 +61,7 @@ class Keys extends React.Component {
           <div className="black-key" id={this.state.notes.Gsharp} />
           <div className="white-key" id={this.state.notes.A} />
           <div className="black-key" id={this.state.notes.Asharp} />
-          <div className="white-key" id={this.state.notes.B} />
+          <div className="white-key" id={this.state.notes.B} /> */}
         </div>
       </div>
     );
