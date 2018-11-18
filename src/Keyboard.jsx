322047e4c8044eb479d3ccf3fc4248b10c7c4f57
Haggis Breakfast <lj.stewart00@gmail.com>
2018-11-18 15:50:10 -0800

    adds drums value to state and onclick function to keyboard component

--- src/Keyboard.jsx
+++ src/Keyboard.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { KeysContainer } from './KeysContainer.jsx';
 
 export class Keyboard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      recording: false,
-      drums: false,
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     recording: false,
+  //   };
+  // }
+  keyClick = (event) => {
+    event.preventDefault();
+    this.props.drums = true;
+    this.props.socket.send(
+      JSON.stringify({
+        drums: this.props.drums,
+      }),
+    );
+  };
   render() {
     return (
       <KeysContainer socket={this.props.socket} notes={this.props.notes} />
