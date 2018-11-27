e0c64a3e44af2f0a2f188aa415e550c700e4ec0d
Haggis Breakfast <lj.stewart00@gmail.com>
2018-11-26 16:37:15 -0800

    removes unused state and constructor in beyboard component

--- src/Keyboard.jsx
+++ src/Keyboard.jsx
@@ -2,12 +2,6 @@ import React from 'react';
 import { KeysContainer } from './KeysContainer.jsx';
 
 export class Keyboard extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      iconVisible: false,
-    };
-  }
   keyClick = (event) => {
     event.preventDefault();
     console.log('play thu drums');
@@ -17,42 +11,34 @@ export class Keyboard extends React.Component {
         type: 'drums',
       }),
     );
-    this.setState({
-      iconVisible: true,
-    });
   };
 
-  // toggle = () => {
-  //   return <img id="mario" src="/images/drummer.gif" />;
-  // };
-
- render() {
-   return (
-     <div>
-
-      <div className="eightbit Keyboard"style={{ background: 'red' }}>
+  render() {
+    return (
+      <div>
+        <div className="eightbit Keyboard" style={{ background: 'red' }}>
           <button
-           className="eightbit OctaveButton"
-           onClick={() => this.props.octaveSwitch('up')}>
-           <img class="key-button" src="/images/octave-up.png" />
+            className="eightbit OctaveButton"
+            onClick={() => this.props.octaveSwitch('up')}>
+            <img class="key-button" src="/images/octave-up.png" />
           </button>
           <button
-           className="eightbit OctaveButton"
-           onClick={() => this.props.octaveSwitch('down')}>
+            className="eightbit OctaveButton"
+            onClick={() => this.props.octaveSwitch('down')}>
             <img class="key-button" src="/images/octave-down.png" />
-            </button>
-           <button className="eightbit DrumButton" onClick={this.keyClick}>
-             Drum
-           </button>
+          </button>
+          <button className="eightbit DrumButton" onClick={this.keyClick}>
+            Drum
+          </button>
           <button
             className="eightbit Triangle"
             onClick={() => this.props.changeWaveform('triangle')}>
-              <img class="key-button" src="/images/triangle.png" />
+            <img class="key-button" src="/images/triangle.png" />
           </button>
           <button
             className="eightbit Square"
             onClick={() => this.props.changeWaveform('triangle')}>
-              <img class="key-button" src="/images/square.png" />
+            <img class="key-button" src="/images/square.png" />
           </button>
           <button
             className="eightbit Sine"
@@ -66,34 +52,35 @@ export class Keyboard extends React.Component {
           </button>
           <KeysContainer
             socket={this.props.socket}
-             notes={this.props.notes}
-             playSound={this.props.playSound}
-           />
-         </div>
-       </div>
-     );
-   }
- }
- 
- // export default Keyboard;
- 
- // var c = new Audio('http://localhost:8000/high-c.mp3');
- // var recording = [
- //   { offset: 1, note: 'C' },
- //   { offset: 1.2, note: 'C' },
- //   { offset: 1.5, note: 'C' },
- //   { offset: 1.6, note: 'C' },
- //   { offset: 1.7, note: 'C' },
- //   { offset: 1.75, note: 'C' },
- //   { offset: 3, note: 'C' },
-  // ];
- 
- // function playNote(i) {
- //   c.play();
- 
- //   setTimeout(() => {
- //     playNote(i++);
- //   }, 1000);
- // }
- 
- // playNote(0);
+            notes={this.props.notes}
+            playSound={this.props.playSound}
+            waveform={this.props.waveform}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+// export default Keyboard;
+
+// var c = new Audio('http://localhost:8000/high-c.mp3');
+// var recording = [
+//   { offset: 1, note: 'C' },
+//   { offset: 1.2, note: 'C' },
+//   { offset: 1.5, note: 'C' },
+//   { offset: 1.6, note: 'C' },
+//   { offset: 1.7, note: 'C' },
+//   { offset: 1.75, note: 'C' },
+//   { offset: 3, note: 'C' },
+// ];
+
+// function playNote(i) {
+//   c.play();
+
+//   setTimeout(() => {
+//     playNote(i++);
+//   }, 1000);
+// }
+
+// playNote(0);
